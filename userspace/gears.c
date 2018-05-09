989b43b163ab20ed76064c46b9d52eb16f2536ab
ozkl <ozkl@users.noreply.github.com>
2018-05-09 19:12:46 +0300

    GL examples

---
+++ userspace/gears.c
@@ -0,0 +1,278 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
+
+#include <GL/gl.h>
+#include <zbuffer.h>
+
+#include <sosousdk.h>
+
+#ifndef M_PI
+#  define M_PI 3.14159265
+#endif
+
+int __errno = 0;
+
+/*
+ * Draw a gear wheel.  You'll probably want to call this function when
+ * building a display list since we do a lot of trig here.
+ *
+ * Input:  inner_radius - radius of hole at center
+ *         outer_radius - radius at center of teeth
+ *         width - width of gear
+ *         teeth - number of teeth
+ *         tooth_depth - depth of tooth
+ */
+static void gear( GLfloat inner_radius, GLfloat outer_radius, GLfloat width,
+                 GLint teeth, GLfloat tooth_depth )
+{
+    GLint i;
+    GLfloat r0, r1, r2;
+    GLfloat angle, da;
+    GLfloat u, v, len;
+
+    r0 = inner_radius;
+    r1 = outer_radius - tooth_depth/2.0;
+    r2 = outer_radius + tooth_depth/2.0;
+
+    da = 2.0*M_PI / teeth / 4.0;
+
+    glShadeModel( GL_FLAT );
+
+    glNormal3f( 0.0, 0.0, 1.0 );
+
+    /* draw front face */
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<=teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+        glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+        glVertex3f( r1*cos(angle), r1*sin(angle), width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 );
+    }
+    glEnd();
+
+    /* draw front sides of teeth */
+    glBegin( GL_QUADS );
+    da = 2.0*M_PI / teeth / 4.0;
+    for (i=0;i<teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+
+        glVertex3f( r1*cos(angle),      r1*sin(angle),      width*0.5 );
+        glVertex3f( r2*cos(angle+da),   r2*sin(angle+da),   width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5 );
+    }
+    glEnd();
+
+    glNormal3f( 0.0, 0.0, -1.0 );
+
+    /* draw back face */
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<=teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+        glVertex3f( r1*cos(angle), r1*sin(angle), -width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+    }
+    glEnd();
+
+    /* draw back sides of teeth */
+    glBegin( GL_QUADS );
+    da = 2.0*M_PI / teeth / 4.0;
+    for (i=0;i<teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5 );
+        glVertex3f( r2*cos(angle+da),   r2*sin(angle+da),   -width*0.5 );
+        glVertex3f( r1*cos(angle),      r1*sin(angle),      -width*0.5 );
+    }
+    glEnd();
+
+    /* draw outward faces of teeth */
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+
+        glVertex3f( r1*cos(angle),      r1*sin(angle),       width*0.5 );
+        glVertex3f( r1*cos(angle),      r1*sin(angle),      -width*0.5 );
+        u = r2*cos(angle+da) - r1*cos(angle);
+        v = r2*sin(angle+da) - r1*sin(angle);
+        len = sqrt( u*u + v*v );
+        u /= len;
+        v /= len;
+        glNormal3f( v, -u, 0.0 );
+        glVertex3f( r2*cos(angle+da),   r2*sin(angle+da),    width*0.5 );
+        glVertex3f( r2*cos(angle+da),   r2*sin(angle+da),   -width*0.5 );
+        glNormal3f( cos(angle), sin(angle), 0.0 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da),  width*0.5 );
+        glVertex3f( r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5 );
+        u = r1*cos(angle+3*da) - r2*cos(angle+2*da);
+        v = r1*sin(angle+3*da) - r2*sin(angle+2*da);
+        glNormal3f( v, -u, 0.0 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da),  width*0.5 );
+        glVertex3f( r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5 );
+        glNormal3f( cos(angle), sin(angle), 0.0 );
+    }
+
+    glVertex3f( r1*cos(0), r1*sin(0), width*0.5 );
+    glVertex3f( r1*cos(0), r1*sin(0), -width*0.5 );
+
+    glEnd();
+
+
+    glShadeModel( GL_SMOOTH );
+
+    /* draw inside radius cylinder */
+    glBegin( GL_QUAD_STRIP );
+    for (i=0;i<=teeth;i++) {
+        angle = i * 2.0*M_PI / teeth;
+        glNormal3f( -cos(angle), -sin(angle), 0.0 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), -width*0.5 );
+        glVertex3f( r0*cos(angle), r0*sin(angle), width*0.5 );
+    }
+    glEnd();
+
+}
+
+
+static GLfloat view_rotx=20.0, view_roty=30.0;
+static GLint gear1, gear2, gear3;
+static GLfloat angle = 0.0;
+
+void draw() {
+    angle += 2.0;
+    glPushMatrix();
+    glRotatef( view_rotx, 1.0, 0.0, 0.0 );
+    glRotatef( view_roty, 0.0, 1.0, 0.0 );
+    //glRotatef( view_rotz, 0.0, 0.0, 1.0 );
+
+    glPushMatrix();
+    glTranslatef( -3.0, -2.0, 0.0 );
+    glRotatef( angle, 0.0, 0.0, 1.0 );
+    glCallList(gear1);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef( 3.1, -2.0, 0.0 );
+    glRotatef( -2.0*angle-9.0, 0.0, 0.0, 1.0 );
+    glCallList(gear2);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef( -3.1, 4.2, 0.0 );
+    glRotatef( -2.0*angle-25.0, 0.0, 0.0, 1.0 );
+    glCallList(gear3);
+    glPopMatrix();
+
+    glPopMatrix();
+}
+
+
+void initScene() {
+    static GLfloat pos[4] = {5.0, 5.0, 10.0, 0.0 };
+    static GLfloat red[4] = {0.8, 0.1, 0.0, 1.0 };
+    static GLfloat green[4] = {0.0, 0.8, 0.2, 1.0 };
+    static GLfloat blue[4] = {0.2, 0.2, 1.0, 1.0 };
+
+    glLightfv( GL_LIGHT0, GL_POSITION, pos );
+    glEnable( GL_CULL_FACE );
+    glEnable( GL_LIGHTING );
+    glEnable( GL_LIGHT0 );
+    glEnable( GL_DEPTH_TEST );
+
+    /* make the gears */
+    gear1 = glGenLists(1);
+    glNewList(gear1, GL_COMPILE);
+    glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, red );
+    gear( 1.0, 4.0, 1.0, 20, 0.7 );
+    glEndList();
+
+    gear2 = glGenLists(1);
+    glNewList(gear2, GL_COMPILE);
+    glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, green );
+    gear( 0.5, 2.0, 2.0, 10, 0.7 );
+    glEndList();
+
+    gear3 = glGenLists(1);
+    glNewList(gear3, GL_COMPILE);
+    glMaterialfv( GL_FRONT, GL_AMBIENT_AND_DIFFUSE, blue );
+    gear( 1.3, 2.0, 0.5, 10, 0.7 );
+    glEndList();
+    glEnable( GL_NORMALIZE );
+}
+
+int main(int argc, char** argv)
+{
+    int winSizeX = 640;
+    int winSizeY = 480;
+    int screenWidth = 1024;
+    int pitch = screenWidth * 4;
+    int	mode = ZB_MODE_RGBA;
+
+    ZBuffer *zBuffer = ZB_open( winSizeX, winSizeY, mode, 0, 0, 0, 0);
+
+    glInit( zBuffer );
+
+    glClearColor (0.0, 0.0, 0.0, 0.0);
+    glViewport (0, 0, winSizeX, winSizeY);
+    glEnable(GL_DEPTH_TEST);
+    GLfloat  h = (GLfloat) winSizeY / (GLfloat) winSizeX;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum( -1.0, 1.0, -h, h, 5.0, 60.0 );
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef( 0.0, 0.0, -45.0 );
+
+    initScene();
+
+    int fd = open("/dev/fb0", 0);
+    if (fd >= 0)
+    {
+        int* buffer = mmap(NULL, 1024*768*4, 0, fd, 0);
+
+        if (buffer != (int*)-1)
+        {
+            unsigned int previousTime = getUptimeMilliseconds();
+            unsigned int frameCounter = 0;
+            while (1)
+            {
+
+                glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+                draw();
+
+                ZB_copyFrameBuffer(zBuffer, buffer, pitch);
+
+                ++frameCounter;
+
+                unsigned int time = getUptimeMilliseconds();
+                unsigned int diff = time - previousTime;
+                if (diff >= 1000)
+                {
+                    printf("%d frames in %d milliseconds\n", frameCounter, diff);
+
+                    previousTime = time;
+                    frameCounter = 0;
+                }
+            }
+        }
+        else
+        {
+            printf("mmap failed\n");
+        }
+    }
+    else
+    {
+        printf("could not open /dev/fb0\n");
+    }
+
+    ZB_close(zBuffer);
+
+    return 0;
+}
