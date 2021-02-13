7fb2fa90398e68413f108a33af3a411189b94cc9
ozkl <ozkl@users.noreply.github.com>
2021-02-13 17:27:41 +0300

    ftruncate syscall added

--- musl-1.2.0/src/unistd/ftruncate.c
+++ musl-1.2.0/src/unistd/ftruncate.c
@@ -3,7 +3,8 @@
 
 int ftruncate(int fd, off_t length)
 {
-	return syscall(SYS_ftruncate, fd, __SYSCALL_LL_O(length));
+	//return syscall(SYS_ftruncate, fd, __SYSCALL_LL_O(length));
+	return syscall(SYS_ftruncate, fd, (int)length);
 }
 
 weak_alias(ftruncate, ftruncate64);
