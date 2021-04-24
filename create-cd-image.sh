8339000940219bad492c35157ba694036a87c52c
ozkl <ozkl@users.noreply.github.com>
2021-04-24 15:32:16 +0300

    image generation scipts updated

--- create-cd-image.sh
+++ create-cd-image.sh
@@ -2,3 +2,5 @@
 cp kernel.bin bootdisk-root/boot/
 cp initrd.fat bootdisk-root/boot/
 grub-mkrescue -o soso.iso bootdisk-root
+
+#grub-pc, xorriso, mtools should have been installed
--- create-initrd.sh
+++ create-initrd.sh
@@ -4,9 +4,9 @@ dd if=/dev/zero of=initrd.fat bs=8M count=1
 LOOP=`losetup -f`
 sudo losetup $LOOP initrd.fat
 sudo mkfs.vfat $LOOP
-sudo mount $LOOP /mnt
-cp -r userspace/bin/* /mnt/
-umount /mnt
-losetup -d $LOOP
+sudo mount $LOOP mnt
+cp -r userspace/bin/* mnt/
+sudo umount mnt
+sudo losetup -d $LOOP
 sync
-chown $SUDO_USER:$SUDO_USER initrd.fat
+sudo chown $SUDO_USER:$SUDO_USER initrd.fat
